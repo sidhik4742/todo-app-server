8bc008ef3645b2a2b0653d777e0a2e1a663abd39
sidhik4742 <sidhik64742@gmail.com>
2020-08-10 13:21:57 +0530

    completed register api with model items

--- index.js
+++ index.js
@@ -8,21 +8,6 @@ const port = process.env.PORT || 3001;
 const Token = require("./middleware/token");
 const router = require("./router/static");
 
-////////////////// modal data from db //////////////////
-// module.exports.data = {
-//   id: "1234567890",
-//   userName: "sidhik",
-//   password: "letmein",
-//   items: [
-    // { itemName: "Boost", quantity: 1, rate: 300 },
-    // { itemName: "Horlicks", quantity: 1, rate: 320 },
-    // { itemName: "Complan", quantity: 1, rate: 280 },
-    // { itemName: "Bournvita", quantity: 1, rate: 250 },
-    // { itemName: "Lays", quantity: 0.1, rate: 5 },
-    // { itemName: "Kurkure", quantity: 0.1, rate: 5 },
-//   ],
-// };
-
 app.use(cors());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
--- router/static.js
+++ router/static.js
@@ -3,20 +3,24 @@ const assert = require("assert");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+// const moduleFun = require("../module");
+
+const middleware = require("../middleware/token");
+const { json } = require("body-parser");
 
 // Mngo DB Connection URL
 const url = "mongodb://localhost:27017";
 
 const saltRounds = 10;
 
-const middleware = require("../middleware/token");
-const modalData = require("../index");
-const { json } = require("body-parser");
-
 //////////////////////////////////////////Api for registration////////////////////////////////////
 router.post("/register", middleware.validation, (req, res) => {
+  // let userDetails = req.body;
+  // let status = moduleFun.registerNewUser(userDetails);
+  // console.log(status);
+  // res.send(status.message);
   //   console.log(req.body);
-  
+
   const encryptedPassword = bcrypt.hashSync(req.body.password, saltRounds);
   let userName = req.body.userName;
   let emailOrPhone = req.body.emailOrPhone;
@@ -30,34 +34,44 @@ router.post("/register", middleware.validation, (req, res) => {
     console.log("Connected successfully to server");
     const dbName = db.db("customerDetails");
     var query = { EmailOrPhone: emailOrPhone };
+
     dbName
       .collection("registerDetails")
       .find(query)
-      .toArray((error, collection) => {
+      .toArray()
+      .then((collection) => {
         // console.log(collection);
         if (collection.length !== 0) {
           res.send("email or phone already registered");
           console.log("email or phone already registered");
           db.close();
         } else {
-          dbName.collection("registerDetails").insertOne(
-            {
-              Username: userName,
-              EmailOrPhone: emailOrPhone,
-              Model: model,
-              Password: encryptedPassword,
-            },
-            (error, collection) => {
-              if (error) {
-                console.error(error);
-                res.send("registration failed please try again");
-              } else {
-                console.log(collection.result);
-                res.send("successfully registered");
-              }
-              db.close();
-            }
-          );
+          dbName
+            .collection(model)
+            .find({})
+            .toArray()
+            .then((items) => {
+              // console.log(items);
+              dbName
+                .collection("registerDetails")
+                .insertOne({
+                  Username: userName,
+                  EmailOrPhone: emailOrPhone,
+                  Model: model,
+                  Password: encryptedPassword,
+                  Items: items,
+                })
+                .then((collection) => {
+                  if (!collection.result.ok) {
+                    console.error("registration failed please try again");
+                    res.send("registration failed please try again");
+                  } else {
+                    console.log(collection.result);
+                    res.send("successfully registered");
+                  }
+                  db.close();
+                });
+            });
         }
       });
   });
